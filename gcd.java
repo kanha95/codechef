c4ea99a0feb321de0a44b7af008cdfbf01cf44a7
Ruturaj Mohanty <kanha95@gmail.com>
2016-11-08 12:11:44 +0530

    GCDoFOneSmallAndOneVeryBigIntegerOf250Digits
    Problem code: GCD2

---
+++ gcd.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+
+public class acm {
+   
+    
+    static  int gcd(int a,int b){
+        if(b==0) return a;
+        
+        return gcd(b,a%b);
+    }
+    
+    public static void main(String[] args)
+    {
+        
+      FasterScanner sc=new FasterScanner(System.in);
+     
+
+        int a=sc.nextInt();
+        
+        String s=sc.nextString();
+        
+        int rem=0;
+        int num=0;
+        int i=0;
+        while(i<s.length()){
+            num=rem*10+Integer.parseInt(""+s.charAt(i));
+            rem=num%a;
+            i++;
+        }
+  
+        System.out.println(gcd(a,rem));
+       
+    
+}
+ 
+}  
+
+class FasterScanner {
+	private InputStream mIs;
+	private byte[] buf = new byte[1024];
+	private int curChar;
+	private int numChars;
+ 
+	public FasterScanner() {
+		this(System.in);
+	}
+ 
+	public FasterScanner(InputStream is) {
+		mIs = is;
+	}
+ 
+	public int read() {
+		if (numChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= numChars) {
+			curChar = 0;
+			try {
+				numChars = mIs.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (numChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+ 
+	public String nextLine() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+ 
+	public String nextString() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isSpaceChar(c));
+		return res.toString();
+	}
+ 
+	public long nextLong() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		do {
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+ 
+	public int nextInt() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+ 
+	public boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+ 
+	public boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+ 
+} 
